3b5a1999f013730b779b825f6393b046a8c960e2
Prasad-Pimparkar <123096749+Prasad-Pimparkar@users.noreply.github.com>
2023-01-21 18:25:07 +0530

    Add files via upload

--- new.py
+++ new.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from dash.dependencies import Output, Input
 
 
+
 app=Dash(__name__)
 
 scrip = pd.read_csv('List_of_securities.csv')
@@ -19,7 +20,7 @@ options = [{'label': row['label'], 'value': row['value']} for index, row in scri
 app.layout = html.Div([
     dcc.Dropdown(id='dropdown', options=options, value=options[0]['value']),
     html.H1(id='header', children=options[0]['value'], style={'text-align':'center'}),
-    dcc.Graph(id='chart', figure=go.Figure(),config={'displaymodebar':False}, style={'height':'1000px'})
+    dcc.Graph(id='chart', figure=go.Figure(),config={'displaymodebar':True, 'scrollZoom':True},style={'height':'1000px'})    
     # html.Div(id='stock_data')
 ])
 
@@ -45,6 +46,9 @@ def update_chart(value):
         df=df.set_index(df.Date)
         fig=go.Figure(go.Candlestick(x=df["Date"],open=df["Open"],high=df["High"],low=df["Low"],close=df["Close"]))
         fig.update_layout(title=f'{value} price history')
+        fig.add_trace(go.Scatter(x=df.index, y=df['Close'].rolling(window=50).mean(),mode='lines', name='MA 50'))
+        fig.add_trace(go.Scatter(x=df.index, y=df['Close'].rolling(window=100).mean(),mode='lines', name='MA 100'))
+        fig.add_trace(go.Scatter(x=df.index, y=df['Close'].rolling(window=200).mean(),mode='lines', name='MA 200'))
         return fig
     except:
         return html.Div("Data not available")
