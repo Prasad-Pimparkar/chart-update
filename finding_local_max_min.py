ed68ab8a55f81a5f7d4447b8e38111b5886c9dd7
Prasad-Pimparkar <123096749+Prasad-Pimparkar@users.noreply.github.com>
2023-01-29 14:48:06 +0530

    Add files via upload

--- finding_local_max_min.py
+++ finding_local_max_min.py
@@ -1,12 +1,14 @@
 import numpy as np
-from scipy.signal import argrelextrema
+from scipy.signal import argrelextrema, find_peaks
 import pandas as pd
 import yfinance as yf
 import plotly.graph_objects as go
+import plotly.io as pio
+pio.renderers.default="browser"
 
 # Stock data
 quote=yf.Ticker('RELIANCE.NS')
-df=quote.history(start=('2022-10-01'), end=('2023-01-20'), interval='1d')
+df=quote.history(start=('2020-10-01'), end=('2023-01-20'), interval='1d')
 df.to_csv('trial_data.csv')
 df=pd.read_csv('trial_data.csv')
 df.Date=pd.to_datetime(df.Date)
@@ -14,34 +16,71 @@ df.set_index(df.Date)
 high_data = df['High']
 low_data = df['Low']
 
+threshhold=2
 high_data = np.array(high_data)
 low_data = np.array(low_data)
 
-# Calculate the derivative using Numpy's diff() function
-high_derivative = np.diff(high_data)
-low_derivative = np.diff(low_data)
+peaks,_=find_peaks(high_data)
+local_max_indices=peaks
+drop_indices=[]
 
-# Find the zero-crossing points
-zero_crossing_indices = np.where(np.diff(np.sign(high_derivative)))[0]
-zero_crossing_indices = np.where(np.diff(np.sign(low_derivative)))[0]
+for i in range(1, len(local_max_indices)):
+    if high_data[local_max_indices[i]] < high_data[local_max_indices[i-1]]:
+        drop_indices.append(local_max_indices[i])
+    elif i+1 < len(local_max_indices):
+        if high_data[local_max_indices[i]] < high_data[local_max_indices[i+1]]:
+            drop_indices.append(local_max_indices[i])
 
-# Identify the local maxima and minima
-local_max_indices = argrelextrema(high_data, np.greater)
-local_min_indices = argrelextrema(low_data, np.less)
+local_max_indices=np.array([i for i in local_max_indices if i not in drop_indices])
 
-local_max_dates = df['Date'][local_max_indices[0]]
-local_min_dates = df['Date'][local_min_indices[0]]
+for p in range(1, len(local_max_indices)):
+    if high_data[local_max_indices[p]] < high_data[local_max_indices[p-1]]:
+        drop_indices.append(local_max_indices[p])
+    elif p+1 < len(local_max_indices):
+        if high_data[local_max_indices[p]] < high_data[local_max_indices[p+1]]:
+            drop_indices.append(local_max_indices[p])
 
-local_max=high_data[local_max_indices[0]]
-local_min=low_data[local_min_indices[0]]
+local_max_indices=np.array([p for p in local_max_indices if p not in drop_indices])
 
+local_max=high_data[local_max_indices]
+local_max_dates=df['Date'][local_max_indices]
+slmax, intermax= np.polyfit(local_max_indices,local_max,1)
+max_y_values=slmax*local_max_indices+intermax
+
+valleys,_=find_peaks(-low_data)
+local_min_indices=valleys
+for j in range(1, len(local_min_indices)):
+    if low_data[local_min_indices[j]] > low_data[local_min_indices[j-1]]:
+        drop_indices.append(local_min_indices[j])
+    elif j+1 < len(local_min_indices):
+        if low_data[local_min_indices[j]] > low_data[local_min_indices[j+1]]:
+            drop_indices.append(local_min_indices[j])
+
+local_min_indices=[j for j in local_min_indices if j not in drop_indices]
+
+for q in range(1, len(local_min_indices)):
+    if low_data[local_min_indices[q]] > low_data[local_min_indices[q-1]]:
+        drop_indices.append(local_min_indices[q])
+    elif q+1 < len(local_min_indices):
+        if low_data[local_min_indices[q]] > low_data[local_min_indices[q+1]]:
+            drop_indices.append(local_min_indices[q])
+
+local_min_indices=[q for q in local_min_indices if q not in drop_indices]
+
+local_min=low_data[local_min_indices]
+local_min_dates=df['Date'][local_min_indices]
+# coef_min= np.polyfit(local_min_indices,local_min,1)
+# min_x_values=np.linspace(min(local_min_dates), max(local_min_dates), 2)
+# min_y_values=coef_min[0]*min_x_values+coef_min[1]
+
+# only to check output will be deleted later
 print("Local maxima position:", local_max_indices)
 print("Local maxima:", local_max)
 print("Local minima position:", local_min_indices)
 print("Local minima:", local_min)
-
+print(peaks)
 fig=go.Figure(go.Candlestick(x=df['Date'], open=df['Open'], high=df['High'], low=df['Low'], close=df['Close']))
-
+fig.update_layout(xaxis_rangeslider_visible=False)
 fig.add_trace(
     go.Scatter(
         x=local_max_dates, 
@@ -57,10 +96,30 @@ fig.add_trace(
         x=local_min_dates, 
         y=local_min, 
         mode='markers', 
-        name='Local_max',
+        name='Local_min',
         marker=dict(size=15, color='green', symbol='x')
+        )
+    )
+
+fig.add_trace(
+    go.Scatter(
+        x=local_max_dates,
+        y=local_max,
+        mode=('lines'),
+        name=('u+pper trend line')
+        )
+    )
+
+
+fig.add_trace(
+    go.Scatter(
+        x=local_min_dates,
+        y=local_min,
+        mode=('lines'),
+        name=('lower trend line')
         )
     )
 
 
 fig.show()
+
--- new.py
+++ new.py
@@ -9,7 +9,8 @@ from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 from datetime import datetime
 from dash.dependencies import Output, Input
-
+import numpy as np
+from scipy.signal import argrelextrema, find_peaks
 
 
 app=Dash(__name__)
@@ -43,14 +44,76 @@ def update_chart(value):
         sd.to_csv('stock_price_data.csv')
         df=pd.read_csv('stock_price_data.csv')
         df.Date=pd.to_datetime(df.Date)
-        df=df.set_index(df.Date)
+        df=df.set_index(df.Date)        
+        high_data = df['High']
+        low_data = df['Low']
+        high_data = np.array(high_data)
+        low_data = np.array(low_data)
+        peaks,_=find_peaks(high_data)
+        local_max_indices=peaks
+        drop_indices=[]
+        for i in range(1, len(local_max_indices)):
+            if high_data[local_max_indices[i]] < high_data[local_max_indices[i-1]]:
+                drop_indices.append(local_max_indices[i])
+            elif i+1 < len(local_max_indices):
+                if high_data[local_max_indices[i]] < high_data[local_max_indices[i+1]]:
+                    drop_indices.append(local_max_indices[i])
+
+        local_max_indices=np.array([i for i in local_max_indices if i not in drop_indices])
+        for p in range(1, len(local_max_indices)):
+            if high_data[local_max_indices[p]] < high_data[local_max_indices[p-1]]:
+                drop_indices.append(local_max_indices[p])
+            elif p+1 < len(local_max_indices):
+                if high_data[local_max_indices[p]] < high_data[local_max_indices[p+1]]:
+                    drop_indices.append(local_max_indices[p])
+        local_max_indices=np.array([p for p in local_max_indices if p not in drop_indices])
+        
+        local_max=high_data[local_max_indices]
+        local_max_dates=df['Date'][local_max_indices]
+        
+        valleys,_=find_peaks(-low_data)
+        local_min_indices=valleys
+        for j in range(1, len(local_min_indices)):
+            if low_data[local_min_indices[j]] > low_data[local_min_indices[j-1]]:
+                drop_indices.append(local_min_indices[j])
+            elif j+1 < len(local_min_indices):
+                if low_data[local_min_indices[j]] > low_data[local_min_indices[j+1]]:
+                    drop_indices.append(local_min_indices[j])
+        local_min_indices=[j for j in local_min_indices if j not in drop_indices]
+        for q in range(1, len(local_min_indices)):
+            if low_data[local_min_indices[q]] > low_data[local_min_indices[q-1]]:
+                drop_indices.append(local_min_indices[q])
+            elif q+1 < len(local_min_indices):
+                if low_data[local_min_indices[q]] > low_data[local_min_indices[q+1]]:
+                    drop_indices.append(local_min_indices[q])
+        local_min_indices=[q for q in local_min_indices if q not in drop_indices]
+        
+        local_min=low_data[local_min_indices]
+        local_min_dates=df['Date'][local_min_indices]
+                
         fig=go.Figure(go.Candlestick(x=df["Date"],open=df["Open"],high=df["High"],low=df["Low"],close=df["Close"]))
-        fig.update_layout(title=f'{value} price history')
+        fig.update_layout(title=f'{value} price history', dragmode='pan', xaxis_rangeslider_visible=False)        
         fig.add_trace(go.Scatter(x=df.index, y=df['Close'].rolling(window=50).mean(),mode='lines', name='MA 50'))
         fig.add_trace(go.Scatter(x=df.index, y=df['Close'].rolling(window=100).mean(),mode='lines', name='MA 100'))
         fig.add_trace(go.Scatter(x=df.index, y=df['Close'].rolling(window=200).mean(),mode='lines', name='MA 200'))
+                        
+        fig.add_trace(go.Scatter(x=local_max_dates, 
+                                  y=local_max,
+                                  mode='markers',
+                                  name='Local_max',
+                                  marker=dict(size=10, color='red', symbol='x'))
+                      )
+                
+        fig.add_trace(go.Scatter(x=local_min_dates, 
+                                  y=local_min, 
+                                  mode=('markers'), 
+                                  name='Local_min', 
+                                  marker=dict(size=10, color='green', symbol='x'))
+                      )
+        
         return fig
-    except:
+    except Exception as e:
+        print(e)
         return html.Div("Data not available")
 
 
